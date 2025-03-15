c0ea1ac4ada5bbbbefe4b7a58d080cae2dc0ae4a
menanashaat <admin@Win-Raag-mna>
2025-03-15 16:58:56 +0200

    install ElementPlus

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import { ElButton, ElTable, ElPagination, ElForm, ElInput, ElSelect, ElOption } from 'element-plus';
 
 createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(ElementPlus);
+app.component('ElButton', ElButton);
+app.component('ElTable', ElTable);
+app.component('ElPagination', ElPagination);
+app.component('ElForm', ElForm);
+app.component('ElInput', ElInput);
+app.component('ElSelect', ElSelect);
+app.component('ElOption', ElOption);
